879272b8fbb0d1539638b188e83aae5647b66458
Eduardo Garza <omaarg@gmail.com>
2020-12-24 14:19:23 -0600

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='banes-ramomar',
-    version='1.0.0',
+    version='1.1.0',
     author='Eduardo Garza',
     author_email='hola@ramomar.dev',
     description='Scrape Banorte transaction emails.',
